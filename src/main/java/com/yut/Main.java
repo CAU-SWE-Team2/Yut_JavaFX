7d8148093a218606096935374a807db0169299a0
daehan <gangdehan@gmail.com>
2025-05-31 17:15:03 +0900

    swing deleted

--- src/main/java/com/yut/Main.java
+++ src/main/java/com/yut/Main.java
@@ -1,20 +1,9 @@
 package com.yut;
 
-import javax.swing.SwingUtilities;
 
-import com.yut.ui.swing.MainFrame;
 import com.yut.controller.TitleController;
 
 public class Main {
     public static void main(String[] args) {
-
-        SwingUtilities.invokeLater(() -> {
-
-            MainFrame mainFrame = new MainFrame();
-
-            TitleController titleController = new TitleController(mainFrame);
-
-            titleController.start();
-        });
     }
 }
